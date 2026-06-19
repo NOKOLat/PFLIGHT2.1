5e4f6db6f3426e440272e66be128603179d16480
aoi-netai <aoi256jp@Gmail.com>
2026-06-19 18:20:49 +0900

    ループ時間測定コードを追加

--- Core/Src/wrapper.cpp
+++ Core/Src/wrapper.cpp
@@ -11,14 +11,17 @@ std::optional<LoopManager> loop_manager;
 
 void init(){
 
-	printf("Build: 1\n");
-	loop_manager.emplace();
+    printf("Build: 1\n");
+    loop_manager.emplace();
     state_manager.init();
 }
 
 void loop(){
 
     static bool just_ran = false;
+    static unsigned long accumulated_loop_time_us = 0;
+    static uint32_t measured_loop_count = 0;
+    constexpr uint32_t average_loop_count = 100;
     const unsigned long loop_interval_us = loop_manager->getLoopTime();
 
     if (!loop_manager->isWaitNextLoop()) {
@@ -28,7 +31,23 @@ void loop(){
         }
 
         just_ran = true;
+
+        const uint32_t update_start_cycle = DWT->CYCCNT;
         state_manager.update();
+        const uint32_t update_end_cycle = DWT->CYCCNT;
+        const uint32_t update_delta_cycles = update_end_cycle - update_start_cycle;
+        const unsigned long update_time_us = update_delta_cycles / (SystemCoreClock / 1000000);
+
+        accumulated_loop_time_us += update_time_us;
+        measured_loop_count ++;
+        if(measured_loop_count >= average_loop_count){
+
+            const unsigned long average_loop_time_us = accumulated_loop_time_us / measured_loop_count;
+            printf("[INFO] average update loop time: %lu us / %lu loops\r\n", average_loop_time_us, (unsigned long)measured_loop_count);
+
+            accumulated_loop_time_us = 0;
+            measured_loop_count = 0;
+        }
 
     } else {
         just_ran = false;
